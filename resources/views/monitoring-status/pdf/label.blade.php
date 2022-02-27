9d6bcae7c884d5f5f87d824ea3dfb61e75a105ad
Arif Budiyono <udots23@gmail.com>
2022-02-27 23:37:05 +0700

    Fix Print Label

--- resources/views/monitoring-status/pdf/label.blade.php
+++ resources/views/monitoring-status/pdf/label.blade.php
@@ -12,30 +12,29 @@
         /*width: {{ $width }}pt;
         height: {{ $height }}pt;*/
         /*display: block;*/
-        width: 21cm;
+        width: 11cm;
         height: 16.5cm;
         /*background-color: black;*/
     }
 
     .box {
         display: inline-block;
-        width: 6cm;
-        height: 1.8cm;
+        width: 4cm;
+        height: 3cm;
         padding-right: 33px; 
         padding-top: 6px;
+        margin: 0 3mm 3mm 0;
     }
 
     p {
-        width: 183.25pt;
-        margin-left: 15pt;
+        width: 4cm;
+        margin-left: 1.5mm;
         /*color: white;*/
     }
 </style>
 <body>
     <div class="body">
-        <table style="width: 100%">
-        </table>
-        @for($i = 0; $i < 24; $i++)
+        @for($i = 0; $i < 10; $i++)
            <div class="box">
                 <p>{{ $list['Medrec'] }}<br>
                 {{ $list['Firstname'] }} ({{ $list['KdSex'] }})<br>
