6a3e968ba9cdef2823a1c51dbdafa8c781e629e6
Arif Budiyono <udots23@gmail.com>
2022-02-27 23:59:11 +0700

    Fix Kartu Pasien Print

--- resources/views/master-pasien/print-kartu-pasien.blade.php
+++ resources/views/master-pasien/print-kartu-pasien.blade.php
@@ -11,6 +11,7 @@
     body {
         width: {{ $width }};
         height: {{ $height }};
+        line-height: 2;
     }
 
     .rekammedis {
@@ -19,9 +20,19 @@
 
     .box {
         position: fixed;
-        top: 3cm;
-        padding-left: 0.5cm;
+        top: 0.5cm;
+        padding-left: 1.5cm;
         padding-bottom: 0.5cm;
+    }
+
+    .text-center {
+        text-align: center;
+    }
+
+    .text-right {
+        text-align: right;
+        padding-right: 0.5cm;
+        margin-top: 10px;
     }
 
     .page-break {
@@ -29,8 +40,15 @@
     }
 </style>
 <body>
-   <div class="box"><b style="font-size: 16px;">{{ $list->Medrec }}</b><br>
-        <b>{{ $list->Firstname }}</b><br><b>
-        {{ $list->Address }}</b><br><img src="data:image/png;base64,{{DNS1D::getBarcodePNG($list->Medrec, 'C39')}}" height="25 " width="120">
+   <div class="box">
+       <div class="text-center">
+            <b style="font-size: 14px;">{{ $list->Medrec }}</b><br>
+            <b style="font-size: 16px;">{{ $list->Firstname }}</b><br>
+            <b style="font-size: 14px;">{{ date('d-m-Y', strtotime($list->Bod)) }}</b><br>
+            <b style="font-size: 14px; margin-top: 10px;">{{ $list->KdSex == 'P' ? 'Perempuan' : 'Laki-laki' }}</b>
+       </div>
+       <div class="text-right">
+           <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($list->Medrec, 'C39')}}" height="25 " width="120">
+       </div>
     </div>
 </body>
