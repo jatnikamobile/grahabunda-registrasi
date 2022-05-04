29ff1973a8b1c5c70543b7c9fc050c451dae997e
Arif Budiyono <udots23@gmail.com>
2022-05-04 17:26:53 +0700

    Add Log Login

--- app/Http/Controllers/AuthDbpass/LoginController.php
+++ app/Http/Controllers/AuthDbpass/LoginController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 // use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Log;
 
 class LoginController extends Controller
 {
